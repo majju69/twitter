b77cb9f4e39b3b1c008670d88314357821bde73d
Kush Mazumdar <mazumdarkush2002@gmail.com>
2025-07-20 04:00:29 +0530

    1-post routes and controllers added

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,13 +1,15 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
-import { commentOnPost, createPost, deletePost, getAllPosts, getLikedPosts, likeUnlikePost } from "../controllers/post.controller.js";
+import { commentOnPost, createPost, deletePost, getAllPosts, getFollowingPosts, getLikedPosts, getUserPosts, likeUnlikePost } from "../controllers/post.controller.js";
 
 const router=express.Router();
 
 router.use(protectRoute);
 
 router.get("/all",getAllPosts);
+router.get("/following",getFollowingPosts);
 router.get("/likes/:id",getLikedPosts);
+router.get("/user/:username",getUserPosts);
 router.post("/create",createPost);
 router.post("/like/:id",likeUnlikePost);
 router.post("/comment/:id",commentOnPost);
